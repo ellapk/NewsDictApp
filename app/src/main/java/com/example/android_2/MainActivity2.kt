4acebe513da7f2cb0d36f438fed2eb2d9d59ec77
soohyunsky <68281361+soohyunsky@users.noreply.github.com>
2022-01-20 22:12:30 +0900

    activity_main2.xml 이랑 MainActivity2.kt 다시 수정

--- app/src/main/java/com/example/android_2/MainActivity2.kt
+++ app/src/main/java/com/example/android_2/MainActivity2.kt
@@ -1,24 +1,53 @@
 package com.example.android_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
-import android.widget.ScrollView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
 import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
-    lateinit var search: SearchView
-    lateinit var Dictionary: ScrollView
+    lateinit var search: EditText
+    lateinit var searchButton: Button
+    lateinit var dictionary: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        dictionary.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+        dictionary.loadUrl("https://www.doopedia.co.kr/index.do")
+
+        search.setOnEditorActionListener {_, actionId , _ ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                dictionary.loadUrl(search.text.toString())
+                true
+            } else {
+                false
+            }
+        }
 
     }
+
+    override fun onBackPressed() {
+        if(dictionary.canGoBack()) {
+            dictionary.goBack()
+        }else {
+            super.onBackPressed()
+        }
+    }
+
+
+
 
 
 }
