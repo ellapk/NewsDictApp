d982327114f83500784ace5ac559e882669d676f
Leejaeyeon28 <yeon8754619@naver.com>
2022-01-24 18:46:44 +0900

    technology/science 분야 카테고리 구분

--- app/src/main/java/com/example/android_2/MainActivity.kt
+++ app/src/main/java/com/example/android_2/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.android_2
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,30 +14,50 @@ import com.example.android_2.Models.NewsHealines
 
 
 
-class MainActivity : AppCompatActivity(), SelectListener {
+class MainActivity : AppCompatActivity(), SelectListener, View.OnClickListener {
+
     var mRecyclerView: RecyclerView? = null
     var mAdapter: CustomAdapter? = null
     var mLayoutManager : RecyclerView.LayoutManager? = null
     var dialog: ProgressDialog? = null
 
+    var btn_technology: Button? = null
+    var btn_science: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        dialog = ProgressDialog(this)
-        dialog!!.setTitle("Fetching news articles..") //기사를 가져오는중이라고 로딩에 대한 대화상자 띄우기
+        dialog = ProgressDialog(this,R.style.AppCompatAlertDialogStyle)
+
+
+        dialogSettings()
+        dialog!!.setMessage("로딩 중입니다.\n"+"잠시만 기다려주십시오.")
         dialog!!.show()
+
+        btn_technology = findViewById(R.id.btn_technology)
+        btn_technology?.setOnClickListener(this)
+        btn_science = findViewById(R.id.btn_science)
+        btn_science?.setOnClickListener(this)
+
         val manager = RequestManager(this)
         manager.getNewsHeadlines(listener, "technology", null) //여기서 뉴스 기사에 대한 응답을 얻는다
-
     }
 
     private val listener: OnFetchDataListener<NewsApiResponse?> = object : OnFetchDataListener<NewsApiResponse?> {
         override fun onFetchData(list: List<NewsHealines?>?, message: String?) {
-            showNews(list as List<NewsHealines>) //리사이클러뷰에 뉴스를 표시한 후에 dialog 종료
-            dialog?.dismiss()
+
+            if(list?.isEmpty()!!){
+                Toast.makeText(this@MainActivity, "에러 발생", Toast.LENGTH_LONG).show()
+            }
+            else{
+                showNews(list as List<NewsHealines>) //리사이클러뷰에 뉴스를 표시한 후에 dialog 종료
+                dialog?.dismiss()
+            }
+
         }
 
-        override fun onError(message: String?) {}
+        override fun onError(message: String?) {
+            Toast.makeText(this@MainActivity, "에러 발생", Toast.LENGTH_LONG).show()}
     }
 
     private fun showNews(list: List<NewsHealines>) { // 본문을 초기화하는 역할
@@ -48,10 +71,26 @@ class MainActivity : AppCompatActivity(), SelectListener {
 
     }
 
-    override fun OnNewsClicked(healines: NewsHealines?) { //카드뷰를 누르면 다른 액티비티로 넘어가도록 작성한다.
 
+    override fun OnNewsClicked(healines: NewsHealines?) {
         val intent = Intent(this@MainActivity, Dictionary::class.java)
-        intent.putExtra("data", healines) //헤드라인 객체를 webViewActivity에 전달한다.
+        intent.putExtra("data", healines) //헤드라인 객체를 DetailsActivity에 전달한다.
         startActivity(intent)
+    }
+
+    override fun onClick(view: View) {
+        val button = view as Button
+        val category = button.text.toString()
+
+        dialogSettings()
+        dialog!!.setMessage(category + " 분야를 로딩 중입니다.")
+        dialog!!.show()
+        val manager = RequestManager(this)
+        manager.getNewsHeadlines(listener, category, null) //여기서 뉴스 기사에 대한 응답을 얻는다
+    }
+
+    fun dialogSettings(){
+        dialog!!.setTitle(R.string.loading)
+        dialog!!.setIcon(R.drawable.icon_dialog)
     }
 }
