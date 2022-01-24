98cb5b6da8ff9ae47aa0d8b7e0b0dc860609ab28
Leejaeyeon28 <yeon8754619@naver.com>
2022-01-24 19:44:29 +0900

    로딩창, 카테고리별로 뉴스 가져오기

--- app/src/main/java/com/example/android_2/Dictionary.kt
+++ app/src/main/java/com/example/android_2/Dictionary.kt
@@ -1,6 +1,8 @@
 package com.example.android_2
 
+import android.app.ProgressDialog
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
@@ -16,6 +18,7 @@ class Dictionary : AppCompatActivity() {
     lateinit var news: WebView
     lateinit var dictionary: WebView
     var healines: NewsHealines? = null
+    var dialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -25,6 +28,7 @@ class Dictionary : AppCompatActivity() {
         val news = findViewById<WebView>(R.id.news)
         dictionary = findViewById<WebView>(R.id.dictionary)
         healines = intent.getSerializableExtra("data") as NewsHealines
+        dialog = ProgressDialog(this, R.style.AppCompatAlertDialogStyle)
 
         dictionary.apply {
             settings.javaScriptEnabled = true
@@ -35,9 +39,29 @@ class Dictionary : AppCompatActivity() {
 
         news?.apply {
             settings.javaScriptEnabled = true
-            webViewClient = WebViewClient()
-        }
+            webViewClient = object : WebViewClient() {
+                // 1) 로딩 시작
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    dialogSettings()
+                    dialog!!.setMessage("로딩 중입니다.\n" + "잠시만 기다려주십시오.")
+                    dialog!!.show()
+                }
 
+                // 2) 로딩 끝
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    dialog!!.dismiss()
+                }
+
+                // 3) 외부 브라우저가 아닌 웹뷰 자체에서 url 호출
+                override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+                    view.loadUrl(url)
+                    return true
+                }
+
+            }
+        }
         news?.loadUrl(healines?.getUrl())
 
 
@@ -51,8 +75,9 @@ class Dictionary : AppCompatActivity() {
         }
     }
 
-
-
-
+    fun dialogSettings(){
+        dialog!!.setTitle(R.string.loading)
+        dialog!!.setIcon(R.drawable.icon_dialog)
+    }
 
 }
