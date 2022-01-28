d4f534e149dc0f11ae1cf0cfd092772b0eb2d01d
xlebin <ssysjy0302@gmail.com>
2022-01-28 10:28:41 +0900

    SplashActivity 코틀린 파일로 변경

--- app/src/main/java/com/example/android_2/SplashActivity.kt
+++ app/src/main/java/com/example/android_2/SplashActivity.kt
@@ -1,20 +1,18 @@
-package com.example.android_2;
+package com.example.android_2
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
 
-public class SplashActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-
-        try{
-            Thread.sleep(4000); // 4초 기다림
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+class SplashActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            Thread.sleep(4000) // 4초 기다림
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
         }
-        startActivity(new Intent(this,ChooseActivity.class));
-        finish();
+        startActivity(Intent(this, ChooseActivity::class.java))
+        finish()
     }
 }
